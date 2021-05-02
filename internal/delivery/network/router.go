588cb61946735c9671ec0eb0bee899d3ec423bc1
Yu-Jack <jk82421@gmail.com>
2021-05-02 22:14:00 +0800

    feature: add trailing slash in route

--- internal/delivery/network/router.go
+++ internal/delivery/network/router.go
@@ -7,8 +7,8 @@ import (
 func (network *Network) BuildRouter(mux *http.ServeMux) {
 	fs := http.FileServer(http.Dir("./public"))
 	mux.Handle("/", fs)
-	mux.HandleFunc("/todo-list/get", network.GetList)
-	mux.HandleFunc("/todo-list/create", network.CreateList)
-	mux.HandleFunc("/todo-list/delete", network.DeleteList)
-	mux.HandleFunc("/users", network.GetUsers)
+	mux.HandleFunc("/todo-list/get/", network.GetList)
+	mux.HandleFunc("/todo-list/create/", network.CreateList)
+	mux.HandleFunc("/todo-list/delete/", network.DeleteList)
+	mux.HandleFunc("/users/", network.GetUsers)
 }
